86f3ece1719c644571270e8326f56f5fb0f949c9
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-03-31 17:47:00 +0200

    Added Unit Tests For the DiscountCalculator Class

--- Assignment 1/JFree/src/test/DiscountCalculatorTest.java
+++ Assignment 1/JFree/src/test/DiscountCalculatorTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
+
 public class DiscountCalculatorTest {
 
     @Test
@@ -19,12 +20,41 @@ public class DiscountCalculatorTest {
         Week week = new Week(date);
 
         // Act
-
-
+        DiscountCalculator discountCalculator = new DiscountCalculator(week);
+        boolean isSpecialWeek = discountCalculator.isTheSpecialWeek();
         // Assert
+        assertFalse(isSpecialWeek);
 
     }
 
     // Test missing cases ( JUNE, 23 is a date in week 26 )
+    @Test
+    public void testIsTheSpecialWeekWhenTrue() throws Exception {
+        // Arrange
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2025, Calendar.JUNE, 23);  // June 23, 2025
+        Date date = calendar.getTime();
+        Week week = new Week(date);
 
+        // Act
+        DiscountCalculator discountCalculator = new DiscountCalculator(week);
+        boolean isSpecialWeek = discountCalculator.isTheSpecialWeek();
+        // Assert
+        assertTrue(isSpecialWeek);
+
+    }
+
+    @Test
+    public void testGetDiscountPercentageWhenEven() {
+        Week week = new Week(10, 2025);
+        DiscountCalculator discountCalculator = new DiscountCalculator(week);
+        assertEquals(7, discountCalculator.getDiscountPercentage());
+    }
+
+    @Test
+    public void testGetDiscountPercentageWhenOdd() {
+        Week week = new Week(9, 2025);
+        DiscountCalculator discountCalculator = new DiscountCalculator(week);
+        assertEquals(5, discountCalculator.getDiscountPercentage());
+    }
 }
